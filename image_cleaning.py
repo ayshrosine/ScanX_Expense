e2e73c7ac0f8571aeb78d2315a418e68fad50616
ayshrosine <ayushsatyawanbisen@gmail.com>
2026-07-29 21:01:58 +0530

    done  with  image  cleaning

--- image_cleaning.py
+++ image_cleaning.py
@@ -17,15 +17,20 @@ def image_cleaning(input_folder, output_folder):
 
             try: 
                 color_image = cv2.imread(input_path)
+
                 #converting  the color image to grayscale / black and white image
                 gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+
                 # removing  noise from the image using Gaussian blur
                 blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+
                 #otsu's  binarization method to convert the image to binary
                 ret , binary_image = cv2.threshold(blurred_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
                 cv2.imwrite(output_path, binary_image)
                 converted_images += 1
                 print(f"Converted {filename} to black and white and saved to {output_path}")
+                
             except Exception as e:
                 print(f"Error processing {filename}: {e}")
 
